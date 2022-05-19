d13c0b4a224d20dff1c6f1eb0d154cd8e6524758
mzaini30 <muhzaini30@gmail.com>
2022-05-20 03:31:05 +0800

    Kirim

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,6 @@
 	<div class="prose p-3">
 		<h1>Assalamualaikum</h1>
 		<p>Welcome to this Vue template, cooked by Zen.</p>
-		<p>Visit <router-link to="/about">about</router-link>
+		<p>Visit <router-link to="/about">about</router-link></p>
 	</div>
 </template>
